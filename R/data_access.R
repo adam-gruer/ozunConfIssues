8e8635202515f40e586e6947b4c8ba3d13531acd
Adam Gruer <abbot.grover@gmail.com>
2017-10-22 06:53:33 +1100

    initial access to api and conversion of response to data frame

---
+++ R/data_access.R
@@ -0,0 +1,45 @@
+#' get_api_response
+#'
+#' @param url
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_api_response <- function(url = "https://api.github.com/repos/ropensci/ozunconf17/issues",
+                    ...) {
+  #todo: what happens when more than one page
+   httr::GET(url,
+       query = list(state = "all", per_page = 100, page = 1))
+}
+
+
+
+#' get_json_data
+#'
+#' @param response
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_json_data <- function(response) {
+  httr::content(response, type = "text")
+
+}
+
+
+#' parse_json
+#'
+#' @param jsondata
+#'
+#' @return
+#' @export
+#'
+#' @examples
+parse_json <- function(jsondata) {
+  jsonlite::fromJSON(jsondata, flatten = TRUE)
+
+
+}
