3f5deab86a7d3391f807166a6bfa336cd4475f54
Adam Gruer <abbot.grover@gmail.com>
2017-11-12 18:27:28 +1100

    more changes to graphql

--- R/graphql.R
+++ R/graphql.R
@@ -3,9 +3,12 @@
 library("ghql")
 library("jsonlite")
 library("httr")
+library(dplyr)
+library(purrr)
 
 # create graphql client ----
-token <-  github_pat()
+token <-  Sys.getenv('GITHUB_PAT') #github_pat()
+
 cli <- GraphqlClient$new(
   url = "https://api.github.com/graphql",
   headers = add_headers(Authorization = paste0("Bearer ",token ))
@@ -28,120 +31,114 @@ qry
 #cli$exec(qry$queries$myquery)
 qry <- Query$new()
 
-ozunconf_issues_raw$data$repository$issues$edges$issue[3,]
-qry$query('ozunconfissues', '{
-  rateLimit {
-          nodeCount
-          limit
-          cost
-          remaining
-          resetAt
-          }
-          repository(owner: "ropenSci", name: "ozunconf17") {
-          nameWithOwner
-          createdAt
-          description
-          id
-          url
-          watchers(first: 1) {
-          totalCount
-          }
-          stargazers(first: 1) {
-          totalCount
-          }
-          issues(first: 50) {
-          edges {
-          cursor
-          issue: node {
-          id
-          title
-          url
-          state
-          createdAt
-          number
-          bodyHTML
-          bodyText
-          closed
-          author {
-          login
-          url
-          resourcePath
-          avatarUrl
-          }
-          labels(first: 10) {
-          totalCount
-          edges {
-          cursor
-          node {
-          id
-          name
-          }
-          }
-          }
-          reactionGroups {
-          content
-          createdAt
-          users(first: 20) {
-          totalCount
-          edges {
-          user: node {
-          id
-          name
-          }
-          }
-          }
-          }
-          comments(first: 30) {
-          totalCount
-          edges {
-          comment: node {
-          id
-          bodyHTML
-          bodyText
-          createdAt
-          author {
-          login
-          url
-          resourcePath
-          avatarUrl
-          }
-          reactionGroups {
-          content
-          createdAt
-          users(first: 10) {
-          totalCount
-          edges {
-          user: node {
-          id
-          name
-          }
-          }
-          }
-          }
-          }
-          }
-          }
-          }
-          }
-          pageInfo {
-          endCursor
-          hasNextPage
-          hasPreviousPage
-          startCursor
-          }
-          totalCount
-          }
-          }
-          }
-          ')
 
-
-
-
+qry$query('ozunconfissues',
+          '{
+            rateLimit {
+              nodeCount
+              limit
+              cost
+              remaining
+              resetAt
+            }
+            repository(owner: "ropenSci", name: "ozunconf17") {
+              nameWithOwner
+              createdAt
+              description
+              id
+              url
+              watchers(first: 1) {
+                totalCount
+              }
+              stargazers(first: 1) {
+                totalCount
+              }
+              issues(first: 50) {
+                nodes {
+                  id
+                  title
+                  url
+                  state
+                  createdAt
+                  number
+                  bodyHTML
+                  bodyText
+                  closed
+                  author {
+                    login
+                    url
+                    resourcePath
+                    avatarUrl
+                  }
+                  labels(first: 10) {
+                    totalCount
+                    nodes {
+                      id
+                      name
+                    }
+                  }
+                  reactionGroups {
+                    content
+                    createdAt
+                    users(first: 20) {
+                      totalCount
+                      nodes {
+                        id
+                        name
+                      }
+                    }
+                  }
+                  comments(first: 30) {
+                    totalCount
+                    nodes {
+                      id
+                      bodyHTML
+                      bodyText
+                      createdAt
+                      author {
+                        login
+                        url
+                        resourcePath
+                        avatarUrl
+                      }
+                      reactionGroups {
+                        content
+                        createdAt
+                        users(first: 10) {
+                          totalCount
+                          nodes {
+                            id
+                            name
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+                totalCount
+              }
+            }
+          }')
 
 ozunconf_issues_raw <- cli$exec(qry$queries$ozunconfissues)
 
-qry$queries$ozunconf_issues
+issues <- ozunconf_issues_raw$data$repository$issues$nodes
+names(issues)
+map(issues, class)
+x <- map_df(ozunconf_issues_raw,list("repository","issues","nodes"))
+
+issues <- ozunconf_issues_raw$data$repository$issues$edges$issue
+issues$comments$edges[[2]]$comment %>% names()
+
+
+
+str(ozunconf_issues_raw)
 
 
 # make request ----
